6b827d199821428f9fc5755c0511b07864098730
Lorraine Williams <lorrainewilliams2604@gmail.com>
2021-09-03 14:15:40 +0100

    first edition of website

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+
+// Example function
+
+function example() {
+    let myUl = document.getElementById("test");
+
+    if (myUl.style.display == "flex") {
+        myUl.style.display = "none";
+    } else {
+        myUl.style.display = "flex";
+    }
+
+}
+
+
