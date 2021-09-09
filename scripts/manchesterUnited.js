e824f153d9ad348745209fbd7fd29db318b531e1
Lorraine Williams <lorrainewilliams2604@gmail.com>
2021-09-09 16:52:02 +0100

    changed text on button press to match changed image

--- scripts/manchesterUnited.js
+++ scripts/manchesterUnited.js
@@ -28,17 +28,25 @@
 const image1 = document.getElementById("image1");
 const hideShow = document.getElementById("hideShow");
 const image2 = document.getElementById("image2");
+// const text = document.getElementsByClassName("title");
 
+function changeText(text) {
+    document.getElementById("text").innerHTML = text;
+}
 
 hideShow.addEventListener("click", () => {
     if (image1.style.display == "none") {
         image1.style.display = "block";
         hideShow.textContent = "hide";
         image2.style.display = "none";
+        changeText("This is the best view in the world. You may, however, not agree. If this is the case, please click the button.");
     } else {
         image1.style.display = "none";
         hideShow.textContent = "show";
         image2.style.display = "block";
+        changeText("Click show if you change your mind.");
+        // text.innerHTML = "Hello";
+
 
     }
 
